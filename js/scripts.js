515f42444d47f8dc182aa2ce83fbaf3ece6a6e38
laulaucia <buitrago.laura@gmail.com>
2020-05-24 15:04:03 -0700

    adding new js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,53 @@
-(function($){
-  $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    
-    
+document.addEventListener( 'DOMContentLoaded', function () {
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+var secondarySlider = new Splide( '#secondary-slider', {
+	fixedWidth  : 100,
+	fixedHeight : 64,
+	isNavigation: true,
+	gap         : 10,
+	focus       : 'center',
+	pagination  : false,
+	cover       : true,
+	pauseOnFocus: false,
+        autoplay: true,
+	breakpoints : {
+		'600': {
+			fixedWidth  : 66,
+			fixedHeight : 40,
+		}
+	}
+} ).mount();
+
+// Create the main slider.
+var primarySlider = new Splide( '#primary-slider', {
+	type       : 'fade',
+	heightRatio: 0.5,
+	pagination : false,
+	arrows     : false,
+	cover      : true,
+} );
+
+// Set the thumbnails slider as a sync target and then call mount.
+primarySlider.sync( secondarySlider ).mount();
+
+// modal stuff
+    
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    
+    setTimeout(function(){ modal.style.display = "block";}, 3000);
+    
+        // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+	
+	} );
